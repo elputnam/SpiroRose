7e4302ff82c2303a6021f467dbf584f4d9ae073d
EL Putnam <elputnam@inaction.ie>
2022-02-24 12:00:01 +0000

    upated for creating video

--- js/sketch.js
+++ js/sketch.js
@@ -9,9 +9,14 @@ var n = 1;
 var sliderD;
 var sliderN;
 
+var capturer = new CCapture({
+  format:'webm', 
+  framerate: 15
+});
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // createCanvas(1080, 1920);
+  // createCanvas(windowWidth, windowHeight);
+  createCanvas(1920, 1080);
   frameRate(15);
   // sliderD = createSlider(1, 20, 10, 1);
   // sliderN = createSlider(1, 20, 10, 1);
@@ -24,6 +29,7 @@ function draw() {
   // n = sliderN.value();
   var k = n / d;
   background(255);
+  if (frameCount==1) capturer.start(); // start the animation capture
   push();
   translate(width / 2, height / 2);
 
@@ -34,7 +40,8 @@ function draw() {
   for (var a = 0; a < TWO_PI * reduceDenominator(n, d); a += 0.02) {
     var r = 200 * sin(k * a);
     var x = r * sin(a);
-    var y = r * tan(a);
+    var y = r * cos(a);
+    fill(random(255), 0, random(255))
     vertex(x, y);
   }
   endShape(CLOSE);
@@ -51,6 +58,14 @@ function draw() {
   if (d <= 1){
     d = 20;
   }
+  capturer.capture(document.getElementById('defaultCanvas0')); 
+  if (frameCount==900){
+    save_record();
+  }
+}
+
+function save_record() {
+  capturer.save();
 }
 
 function reduceDenominator(numerator, denominator) {
